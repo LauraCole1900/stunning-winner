ed936193aebba26d0b9f1d2d7b354d750a081a0c
Laura Cole <lauracole1900@comcast.net>
2023-02-22 12:18:52 -0700

    commented out unused vars

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
